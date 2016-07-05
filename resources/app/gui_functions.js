613ad5299dd4bf8cdeb62cd4dda0605e0838aba1
idzn <idzn@yandex.ru>
2016-07-05 14:17:25 +0300

    update special paste functions

--- resources/app/gui_functions.js
+++ resources/app/gui_functions.js
@@ -614,44 +614,10 @@ function alterKeymap(str, from, to) {
     return str;
 }
 
-function insertAtCaret(areaId,text) {
-    var txtarea = document.getElementById(areaId);
-    var scrollPos = txtarea.scrollTop;
-    var strPos = 0;
-    var br = ((txtarea.selectionStart || txtarea.selectionStart == '0') ?
-        "ff" : (document.selection ? "ie" : false ) );
-    if (br == "ie") {
-        txtarea.focus();
-        var range = document.selection.createRange();
-        range.moveStart ('character', -txtarea.value.length);
-        strPos = range.text.length;
-    }
-    else if (br == "ff") strPos = txtarea.selectionStart;
-
-    var front = (txtarea.value).substring(0,strPos);
-    var back = (txtarea.value).substring(strPos,txtarea.value.length);
-    txtarea.value=front+text+back;
-    strPos = strPos + text.length;
-    if (br == "ie") {
-        txtarea.focus();
-        var range = document.selection.createRange();
-        range.moveStart ('character', -txtarea.value.length);
-        range.moveStart ('character', strPos);
-        range.moveEnd ('character', 0);
-        range.select();
-    }
-    else if (br == "ff") {
-        txtarea.selectionStart = strPos;
-        txtarea.selectionEnd = strPos;
-        txtarea.focus();
-    }
-    txtarea.scrollTop = scrollPos;
-}
-
 function pasteLine() {
     if (activeScreen == 'edit') {
         if ($('#body').is(':focus')) {
-            insertAtCaret('body', '-------------------------------------------\n');
+            $('#body').insertAtCaret('-------------------------------------------\n');
         }
     }
 }
@@ -659,7 +625,7 @@ function pasteLine() {
 function pasteDoubleLine() {
     if (activeScreen == 'edit') {
         if ($('#body').is(':focus')) {
-            insertAtCaret('body', '===========================================\n');
+            $('#body').insertAtCaret('===========================================\n');
         }
     }
 }
@@ -667,7 +633,7 @@ function pasteDoubleLine() {
 function pastePseudoCheckbox() {
     if (activeScreen == 'edit') {
         if ($('#body').is(':focus')) {
-            insertAtCaret('body', '[ ] ');
+            $('#body').insertAtCaret('[ ] ');
         }
     }
 }
@@ -685,7 +651,7 @@ function genPassword(len=20) {
 function pastePassword() {
     if (activeScreen == 'edit') {
         if ($('#body').is(':focus')) {
-            insertAtCaret('body', genPassword(20));
+            $('#body').insertAtCaret(genPassword(20));
         }
     }
 }
