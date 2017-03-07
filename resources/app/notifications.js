fe7b7188d1eb26af919eef7ce63695e54e8132e0
idzn <idzn@yandex.ru>
2017-03-07 16:39:41 +0300

    add today label to notifications

--- resources/app/notifications.js
+++ resources/app/notifications.js
@@ -2,13 +2,21 @@ Vue.component('notification-item', {
     template: '\
     <li class="notification list-inline">\
     <div class="well">\
-        <p class="time"><span class="badge">{{item.category}}</span>&nbsp;{{item.time}}</p>\
+        <p class="time"><span v-show="is_today(item.time)" class="label label-danger">today</span>&nbsp<span class="badge">{{item.category}}</span>&nbsp;{{item.time}}</p>\
         <h2 class="text">{{item.text}}</h2>\
         <p class="details">{{item.details}}</p>\
     </div>\
     </li>\
     ',
-    props: ['item']
+    props: ['item'],
+    methods: {
+        moment: function (something) {
+            return moment(something);
+        },
+        is_today: function (time) {
+            return moment(time, settings.date_format + ' ' + settings.time_format).format(settings.date_format) == moment().format(settings.date_format)
+        }
+    }
 });
 var vmNotifications = new Vue({
     el: '#notifications-area',
